c58fbc3c83e71df038e8e3ad91482e486c9ac633
Alberto Diaz <adiaz@webdoxclm.com>
2025-06-24 23:02:27 -0400

    Initial Commit

---
+++ remeetner/remeetnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  remeetnerApp.swift
+//  remeetner
+//
+//  Created by Alberto Diaz on 24-06-25.
+//
+
+import SwiftUI
+
+@main
+struct remeetnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
